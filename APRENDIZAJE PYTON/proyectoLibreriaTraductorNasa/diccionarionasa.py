561dcba1f1c80f7fe100ab0824059c1cf1a10409
AdrianoGutierrez <37429722+AdrianoGutierrez@users.noreply.github.com>
2025-05-15 22:05:37 -0500

    cambio desde hit realizado a la rama-nueva

--- APRENDIZAJE PYTON/proyectoLibreriaTraductorNasa/diccionarionasa.py
+++ APRENDIZAJE PYTON/proyectoLibreriaTraductorNasa/diccionarionasa.py
@@ -8,6 +8,7 @@ def saludar():
     
     print(f"hola mundo nasa la suma es: ")  
     print("este es el cambio 2 para probar git")
+    print("cambio realaizado al programa desde git... se debe actualizar en local")
 
 saludar()  
 
